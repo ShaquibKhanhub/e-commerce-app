be29d289d9caf21395b5638e1ea5bc323437cfe4
Shaquibkhanhub <shaquibkhanrdx34@gmail.com>
2025-01-29 23:13:21 +0530

    feat: implement product creation form and product store management

--- client/src/components/InputField.jsx
+++ client/src/components/InputField.jsx
@@ -8,8 +8,10 @@ const InputField = ({
   value,
   onChange,
   placeholder = "",
+  inputClassName = "",
   icon: Icon,
   required = false,
+  rows,
 }) => {
   return (
     <div>
@@ -17,22 +19,34 @@ const InputField = ({
         {label}
       </label>
       <div className="mt-1 relative rounded-md shadow-sm">
-        {Icon && (
+        {Icon && !rows && (
           <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
             <Icon className="h-5 w-5 text-gray-400" aria-hidden="true" />
           </div>
         )}
-        <input
-          id={id}
-          type={type}
-          required={required}
-          value={value}
-          onChange={onChange}
-          className={`block w-full px-3 py-2 ${
-            Icon ? "pl-10" : ""
-          } bg-gray-700 border border-gray-600 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm`}
-          placeholder={placeholder}
-        />
+        {rows ? (
+          <textarea
+            id={id}
+            required={required}
+            value={value}
+            onChange={onChange}
+            rows={rows}
+            className={`block w-full px-3 py-2 bg-gray-700 border border-gray-600 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm ${inputClassName}`}
+            placeholder={placeholder}
+          />
+        ) : (
+          <input
+            id={id}
+            type={type}
+            required={required}
+            value={value}
+            onChange={onChange}
+            className={`block w-full px-3 py-2 ${
+              Icon ? "pl-10" : ""
+            } bg-gray-700 border border-gray-600 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm ${inputClassName}`}
+            placeholder={placeholder}
+          />
+        )}
       </div>
     </div>
   );
